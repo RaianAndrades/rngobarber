754afaaa0d21c8e549e67ff82b7a60d548787ba0
Raian de Andrades <raian.andrades@gmail.com>
2020-01-04 19:16:35 -0300

    Input & Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import Input from '~/components/Input';
 import Background from '~/components/Background';
+import Button from '~/components/Button';
 
 export default function SignIn() {
     return (
         <Background>
             <Text>Signin</Text>
+
+            <Input
+                style={{marginTop: 30}}
+                icon="call"
+                placeholder="Digite seu nome"
+            />
+
+            <Button>Entrar</Button>
         </Background>
     );
 }
