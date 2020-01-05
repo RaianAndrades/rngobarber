1f042289049822dd912a36561b19b942f70e1efb
Raian de Andrades <raian.andrades@gmail.com>
2020-01-04 22:29:37 -0300

    Página SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
 
-import Input from '~/components/Input';
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
-import Button from '~/components/Button';
+
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
 
 export default function SignIn() {
     return (
         <Background>
-            <Text>Signin</Text>
+            <Container>
+                <Image source={logo} />
 
-            <Input
-                style={{marginTop: 30}}
-                icon="call"
-                placeholder="Digite seu nome"
-            />
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                    />
 
-            <Button>Entrar</Button>
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Sua senha secreta"
+                    />
+
+                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                </Form>
+
+                <SignLink onPress={() => {}}>
+                    <SignLinkText>Cria conta gratuita</SignLinkText>
+                </SignLink>
+            </Container>
         </Background>
     );
 }
