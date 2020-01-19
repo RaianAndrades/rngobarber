f6f6f294790f797e695c7655d94f99c93294e2ef
Raian de Andrades <raian.andrades@gmail.com>
2020-01-19 16:44:22 -0300

    Atualização de perfil

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -7,7 +7,7 @@ import {updateProfileSuccess, updateProfileFailure} from './actions';
 
 export function* updateProfile({payload}) {
     try {
-        const {name, email, avatar_id, ...rest} = payload.data;
+        const {name, email, ...rest} = payload.data;
 
         const profile = Object.assign(
             {name, email, avatar_id},
