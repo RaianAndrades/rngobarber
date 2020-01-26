17e7116c6699b8a0de6bcc1cb1e820cc3e063fa0
Raian de Andrades <raian.andrades@gmail.com>
2020-01-26 18:49:36 -0300

    Load de agendamentos

--- src/routes.js
+++ src/routes.js
@@ -61,6 +61,7 @@ export default (signedIn = false) =>
                         Profile,
                     },
                     {
+                        resetOnBlur: true,
                         tabBarOptions: {
                             keyboardHidesTabBar: true,
                             activeTintColor: '#FFF',
